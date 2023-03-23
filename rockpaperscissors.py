5f30e6da6b116839f80a6ce67c8bcfc0b242f353
Jordan Jones <jordan.jones@mintellity.com>
2023-03-24 00:51:43 +0100

    added dictionaries and lizard and spock

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,30 +4,48 @@ import random
 
 #recieve user input
 print('select rock, paper, or scissors')
+rock_dict = {'rock':'tie', 'paper':'lose', 'scissors':'win', 'lizard':'win', 'spock':'lose'}
+paper_dict = {'rock':'win', 'paper':'tie', 'scissors':'lose', 'lizard':'lose', 'spock':'win'}
+scissors_dict = {'rock':'lose', 'paper':'win', 'scissors':'tie', 'lizard':'win', 'spock':'lose'}
+lizard_dict = {'rock':'lose', 'paper':'win', 'scissors':'lose', 'lizard':'tie', 'spock': 'win'}
+spock_dict = {'rock':'win', 'paper':'lose', 'scissors':'win', 'lizard':'lose', 'spock':'tie'}
 
 while True:
     user_choices = input("Enter your selection: ").lower()
-    if user_choices == "rock" or user_choices == "paper" or user_choices == "scissors":  
-        break  
+    if user_choices == "rock":
+        current_dict = rock_dict  
+        break
+    elif user_choices == "paper":
+        current_dict = paper_dict
+        break
+    elif user_choices == "scissors":
+        current_dict = scissors_dict
+        break
+    elif user_choices == "lizard":
+        current_dict = lizard_dict
+        break
+    elif user_choices == "spock":
+        current_dict = spock_dict
+        break
 
 
 #receive computer input 
-computer_choices = ["rock", "paper", "scissors"]
+computer_choices = ["rock", "paper", "scissors", "lizard", "spock"]
 computer_choice = random.choice(computer_choices)
 print('computer selects: ' + computer_choice)
 
 
-#calculate the winners
-if computer_choice == 'rock' and user_choices == 'scissors':
-    print('computer wins!')
-elif computer_choice == 'paper' and user_choices == 'rock':
-    print('computer wins!')
-elif computer_choice == 'scissors' and user_choices == 'paper':
-    print('computer wins!')
-elif computer_choice == user_choices:
-    print('Tie!')
+
+#calculate winners
+if user_choices == computer_choice:
+    outcome = 'tie'
 else:
-    print('user wins!')
+    outcome = current_dict[computer_choice] if current_dict[computer_choice] == 'lose' else 'win'
+
+
+print(f'you {outcome}!')
+
+
 
 
 
